0f2b2e2f5f69d74513ea68469ad7ba5c397c6d52
Ari Wiradana <ariwiradana5@gmail.com>
2024-09-26 08:43:06 +0800

    modified:   components/client/elements/button.primary.tsx

--- components/client/elements/button.primary.tsx
+++ components/client/elements/button.primary.tsx
@@ -19,7 +19,9 @@ const ButtonPrimary: FC<ButtonPrimaryProps> = (props) => {
       className={`${montserrat.className} ${
         props.className
       } flex items-center gap-x-3 h-auto w-auto text-nowrap outline-none font-medium py-3 lg:py-4 px-4 lg:px-5 text-sm md:text-lg lg:font-medium transition-colors ease-in-out duration-500 text-white ${
-        props.disabled ? "bg-gray-400" : "bg-primary"
+        props.disabled
+          ? "bg-gray-400 pointer-events-none cursor-not-allowed"
+          : "bg-primary cursor-auto"
       } rounded`}
     >
       {props.icon && props.icon} <span>{props.title}</span>
